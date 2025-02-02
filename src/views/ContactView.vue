378ecb33f6151236c1ceb7846ae835fad9167cb2
meblo_barham <lomouhamedelbachir@gmail.com>
2025-02-02 00:41:32 +0000

    gestion du formulaire

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -5,24 +5,23 @@
             <!-- Heading Section -->
             <div class="heading text-center text-blue">
                 <h1 class="text-3xl sm:text-4xl font-bold mb-6">Contactez-Nous</h1>
+              
                 <div
                     class="contact-icons text-blue-500 flex flex-col sm:flex-row items-center justify-center gap-6 text-center">
                     <div class="flex items-center">
-                        <img src="https://img.icons8.com/material-sharp/24/marker.png" alt="Location" width="25"
-                            height="25" class="mr-2" />
+                        <i class="fa-solid fa-location-dot text-xl mr-2"></i>
                         <span>4 cité Cpi, VDN Dakar</span>
                     </div>
                     <div class="flex items-center">
-                        <img src="https://img.icons8.com/material-rounded/25/phone--v1.png" alt="Phone" width="25"
-                            height="25" class="mr-2" />
-                        <span>+221 77 123 45 67 </span>
+                        <i class="fa-solid fa-phone text-xl mr-2"></i>
+                        <span>+221 77 123 45 67</span>
                     </div>
                     <div class="flex items-center">
-                        <img src="https://img.icons8.com/material-rounded/96/mail.png" alt="Mail" width="25" height="25"
-                            class="mr-2" />
-                        <span> contact@taktylgroup.com.</span>
+                        <i class="fa-solid fa-envelope text-xl mr-2"></i>
+                        <span>contact@taktylgroup.com</span>
                     </div>
                 </div>
+
             </div>
 
             <!-- Form Section -->
@@ -33,13 +32,13 @@
                         <div class="flex flex-col w-full">
                             <label for="full-name" class="text-lg font-medium mb-2">Nom complet</label>
                             <input type="text" name="full-name" id="full-name" placeholder="Saisissez votre nom complet"
-                                class="px-4 py-2 rounded-lg border border-gray-300 focus:ring-2 focus:ring-blue-400 focus:outline-none" />
+                                class="px-4 py-2 rounded-lg border text-gray-500 border-gray-300 focus:ring-2 focus:ring-blue-400 focus:outline-none" />
                         </div>
                         <div class="flex flex-col w-full">
                             <label for="email-id" class="text-lg font-medium mb-2">Email</label>
                             <input type="email" name="email-id" id="email-id"
                                 placeholder="Saisissez votre adresse e-mail"
-                                class="px-4 py-2 rounded-lg border border-gray-300 focus:ring-2 focus:ring-blue-400 focus:outline-none" />
+                                class="px-4 py-2 rounded-lg border text-gray-500 border-gray-300 focus:ring-2 focus:ring-blue-400 focus:outline-none" />
                         </div>
                     </div>
 
@@ -47,14 +46,14 @@
                     <div class="mt-6">
                         <label for="subject" class="text-lg font-medium mb-2">Objet</label>
                         <input type="text" name="subject" id="subject" placeholder="Mentionnez votre domaine d'intérêt"
-                            class="w-full px-4 py-2 rounded-lg border border-gray-300 focus:ring-2 focus:ring-blue-400 focus:outline-none" />
+                            class="w-full px-4 py-2 rounded-lg border text-gray-500 border-gray-300 focus:ring-2 focus:ring-blue-400 focus:outline-none" />
                     </div>
 
                     <!-- Message -->
                     <div class="mt-6">
                         <label for="message" class="text-lg font-medium mb-2">Message:</label>
                         <textarea name="message" id="message" rows="5" placeholder="Rédigez votre message"
-                            class="w-full px-4 py-2 rounded-lg border border-gray-300 focus:ring-2 focus:ring-blue-400 focus:outline-none"></textarea>
+                            class="w-full px-4 py-2 rounded-lg border text-gray-500 border-gray-300 focus:ring-2 focus:ring-blue-400 focus:outline-none"></textarea>
                     </div>
 
                     <!-- Submit Button -->
