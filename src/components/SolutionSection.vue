ba8a5651f5372971553fddcf02ceb23b9e6625e3
meblo_barham <lomouhamedelbachir@gmail.com>
2025-02-04 12:50:33 +0000

    version 1

--- src/components/SolutionSection.vue
+++ src/components/SolutionSection.vue
@@ -10,11 +10,11 @@
 
           <div>
             <div class="max-w-lg md:max-w-none">
-              <h2 class="text-2xl font-semibold text-gray-900 sm:text-3xl">
+              <h2 class="text-2xl font-semibold text-blue sm:text-3xl">
                 Yokoo
               </h2>
 
-              <p class="mt-4 text-gray-700">
+              <p class="mt-4 text-orange-700">
                 Simplifiez votre gestion quotidienne avec Yokoo !
                 Gestion des stocks, facturation, paiements et relances automatisés en un seul outil. Dites adieu aux
                 erreurs et gagnez du temps pour vous concentrer sur la croissance de votre entreprise. La solution
@@ -35,12 +35,12 @@
 
           <div>
             <div class="max-w-lg md:max-w-none">
-              <h2 class="text-2xl font-semibold text-gray-900 sm:text-3xl">
+              <h2 class="text-2xl font-semibold text-blue sm:text-3xl">
                 NAFABI
 
               </h2>
 
-              <p class="mt-4 text-gray-700">
+              <p class="mt-4 text-orange-700">
                 "Prenez le contrôle de vos finances avec NAFABI !
                 Facturation, gestion des dépenses, paie et bien plus encore. Une solution intuitive et puissante pour
                 une gestion financière transparente et efficace. Votre partenaire pour une santé financière optimale !
@@ -66,11 +66,11 @@
 
           <div>
             <div class="max-w-lg md:max-w-none">
-              <h2 class="text-2xl font-semibold text-gray-900 sm:text-3xl">
+              <h2 class="text-2xl font-semibold text-blue sm:text-3xl">
                 Eduxel
               </h2>
 
-              <p class="mt-4 text-gray-700">
+              <p class="mt-4 text-orange-700">
                 Transformez la gestion de votre établissement scolaire avec Eduxel !
                 Gestion académique, financière et administrative simplifiée. Offrez à votre école les outils modernes
                 pour une administration fluide et une meilleure expérience éducative. L'innovation au service de
@@ -88,11 +88,11 @@
         <div class="grid grid-cols-1 gap-4 md:grid-cols-2 md:items-center md:gap-8">
           <div>
             <div class="max-w-lg md:max-w-none">
-              <h2 class="text-2xl font-semibold text-gray-900 sm:text-3xl">
+              <h2 class="text-2xl font-semibold text-blue sm:text-3xl">
                 Leadiz
               </h2>
 
-              <p class="mt-4 text-gray-700">
+              <p class="mt-4 text-orange-700">
                 Boostez vos ventes avec Leadiz !
                 Prospection, gestion de la relation client (PreCRM et CRM) et suivi des opportunités. Maximisez vos
                 conversions et fidélisez vos clients grâce à une solution adaptée aux PME ambitieuses. Votre allié pour
