d159f41ffb5642921d714e69f5e529a5f74b85d7
meblo_barham <lomouhamedelbachir@gmail.com>
2025-02-19 11:59:59 +0000

    recréation d'une nouvelle page de contact

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,7 +1,95 @@
 <template>
-	     <NavBar />
+	<NavBar />
 
-	<div
+
+
+	<div class="contact_us_6" style="margin-top: 15vh;">
+		<div class="responsive-container-block container">
+			<form class="form-box">
+				<div class="container-block form-wrapper">
+					<div class="mob-text">
+						<p class="text-blk contactus-head">
+							Prenez contact
+						</p>
+						<p class="text-blk contactus-subhead">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Felis diam lectus sapien.
+						</p>
+					</div>
+					<div class="responsive-container-block" id="i2cbk">
+						<div class="responsive-cell-block wk-tab-12 wk-mobile-12 wk-desk-12 wk-ipadp-12" id="i10mt-3">
+							<p class="text-blk input-title">
+								NOM COMPLET
+							</p>
+							<input class="input" id="ijowk-3" name="FirstName"
+								placeholder="Entrez votre nom complet...">
+						</div>
+						<div class="responsive-cell-block wk-tab-12 wk-mobile-12 wk-desk-12 wk-ipadp-12" id="ip1yp">
+							<p class="text-blk input-title">
+								EMAIL
+							</p>
+							<input class="input" id="ipmgh-3" name="Email" placeholder="Entrez votre email...">
+						</div>
+						<div class="responsive-cell-block wk-tab-12 wk-mobile-12 wk-desk-12 wk-ipadp-12" id="ih9wi">
+							<p class="text-blk input-title">
+								OBJET
+							</p>
+							<input class="input" id="imgis-3" name="objet" placeholder="Entrez votre objet...">
+						</div>
+						<div class="responsive-cell-block wk-tab-12 wk-mobile-12 wk-desk-12 wk-ipadp-12" id="i634i-3">
+							<p class="text-blk input-title">
+								MESSAGE
+							</p>
+							<textarea class="textinput" id="i5vyy-3" placeholder="Entrez votre message..."></textarea>
+						</div>
+					</div>
+					<button class="submit-btn" id="w-c-s-bgc_p-1-dm-id-2">
+						Envoyer
+					</button>
+				</div>
+			</form>
+			<div class="responsive-cell-block wk-desk-7 wk-ipadp-12 wk-tab-12 wk-mobile-12" id="i772w">
+				<div class="map-part">
+					<p class="text-blk map-contactus-head" id="w-c-s-fc_p-1-dm-id">
+						Nous contacter à l'adresse
+					</p>
+					<p class="text-blk map-contactus-subhead">
+					<div class="dark:text-orange-600 mt-2">
+						<div class="flex items-center mb-3">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+								class="w-5 h-5 mr-2 sm:mr-6">
+								<path fill-rule="evenodd"
+									d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+									clip-rule="evenodd"></path>
+							</svg>
+							<span>4 cité Cpi, VDN Dakar</span>
+						</div>
+						<div class="flex items-center mb-3">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+								class="w-5 h-5 mr-2 sm:mr-6">
+								<path
+									d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z">
+								</path>
+							</svg>
+							<span>+221 77 123 45 67</span>
+						</div>
+						<div class="flex items-center mb-3">
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+								class="w-5 h-5 mr-2 sm:mr-6">
+								<path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
+								<path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
+							</svg>
+							<span>contact@taktylgroup.com</span>
+						</div>
+					</div>
+					</p>
+					<div class="map-box container-block">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- <div
     style="margin-top:15vh;margin-bottom:10vh; background-color: #A7BDBF;"
     class="grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 dark:bg-orange-100 dark:text-orange-800">
 		<div class="flex flex-col justify-between">
@@ -51,18 +139,761 @@
 			</div>
 			<button type="submit" class="w-full p-3 text-sm font-bold tracking-wide bg-blue uppercase rounded dark:bg-white text-white rounded-full rounded-sm dark:text-white-50">Envoyer</button>
 		</form>
-	</div>
-	<Footer/>
+	</div> -->
+	<Footer />
 
 </template>
 
 <script>
-	  import NavBar from '@/components/NavBar.vue';
-	  import Footer from '@/components/Footer.vue';
-	  export default {
-  components: {
-    NavBar,
-    Footer,
-  }
+import NavBar from '@/components/NavBar.vue';
+import Footer from '@/components/Footer.vue';
+export default {
+	components: {
+		NavBar,
+		Footer,
+	}
 };
 </script>
+
+<style scoped>
+.contact_us_6 * {
+	font-family: Nunito, sans-serif;
+}
+
+.contact_us_6 .text-blk {
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+	margin-left: 0px;
+	line-height: 25px;
+}
+
+.contact_us_6 .responsive-cell-block {
+	min-height: 75px;
+}
+
+.contact_us_6 input:focus,
+.contact_us_6 textarea:focus {
+	outline-color: initial;
+	outline-style: none;
+	outline-width: initial;
+}
+
+.contact_us_6 .container-block {
+	min-height: 75px;
+	width: 100%;
+	padding-top: 10px;
+	padding-right: 10px;
+	padding-bottom: 10px;
+	padding-left: 10px;
+	display: block;
+}
+
+.contact_us_6 .responsive-container-block {
+	min-height: 75px;
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: flex-start;
+	margin-top: 0px;
+	margin-right: auto;
+	margin-bottom: 50px;
+	margin-left: auto;
+	padding-top: 0px;
+	padding-right: 0px;
+	padding-bottom: 0px;
+	padding-left: 0px;
+}
+
+.contact_us_6 .responsive-container-block.big-container {
+	padding-top: 10px;
+	padding-right: 30px;
+	width: 35%;
+	padding-bottom: 10px;
+	padding-left: 30px;
+	background-color: #03a9f4;
+	position: absolute;
+	height: 950px;
+	right: 0px;
+}
+
+.contact_us_6 .responsive-container-block.container {
+	position: relative;
+	min-height: 75px;
+	flex-direction: row;
+	z-index: 2;
+	flex-wrap: nowrap;
+	align-items: center;
+	justify-content: center;
+	padding-top: 0px;
+	padding-right: 30px;
+	padding-bottom: 0px;
+	padding-left: 30px;
+	max-width: 1320px;
+	margin-top: 0px;
+	margin-right: auto;
+	margin-bottom: 0px;
+	margin-left: auto;
+}
+
+.contact_us_6 .container-block.form-wrapper {
+	background-color: white;
+	max-width: 450px;
+	text-align: center;
+	padding-top: 50px;
+	padding-right: 40px;
+	padding-bottom: 50px;
+	padding-left: 40px;
+	box-shadow: rgba(0, 0, 0, 0.05) 0px 4px 20px 7px;
+	border-top-left-radius: 6px;
+	border-top-right-radius: 6px;
+	border-bottom-right-radius: 6px;
+	border-bottom-left-radius: 6px;
+	margin-top: 90px;
+	margin-right: 0px;
+	margin-bottom: 60px;
+	margin-left: 0px;
+}
+
+.contact_us_6 .text-blk.contactus-head {
+	font-size: 36px;
+	line-height: 52px;
+	font-weight: 900;
+}
+
+.contact_us_6 .text-blk.contactus-subhead {
+	color: #9c9c9c;
+	width: 300px;
+	margin-top: 0px;
+	margin-right: auto;
+	margin-bottom: 50px;
+	margin-left: auto;
+	display: none;
+}
+
+.contact_us_6 .responsive-cell-block.wk-desk-6.wk-ipadp-6.wk-tab-12.wk-mobile-12 {
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 26px;
+	margin-left: 0px;
+	min-height: 50px;
+}
+
+.contact_us_6 .input {
+	width: 100%;
+	height: 50px;
+	padding-top: 1px;
+	padding-right: 15px;
+	padding-bottom: 1px;
+	padding-left: 15px;
+	border-top-width: 2px;
+	border-right-width: 2px;
+	border-bottom-width: 2px;
+	border-left-width: 2px;
+	border-top-style: solid;
+	border-right-style: solid;
+	border-bottom-style: solid;
+	border-left-style: solid;
+	border-top-color: #eeeeee;
+	border-right-color: #eeeeee;
+	border-bottom-color: #eeeeee;
+	border-left-color: #eeeeee;
+	border-image-source: initial;
+	border-image-slice: initial;
+	border-image-width: initial;
+	border-image-outset: initial;
+	border-image-repeat: initial;
+	font-size: 16px;
+	color: black;
+}
+
+.contact_us_6 .textinput {
+	width: 98%;
+	min-height: 150px;
+	padding-top: 20px;
+	padding-right: 15px;
+	padding-bottom: 20px;
+	padding-left: 15px;
+	border-top-width: 2px;
+	border-right-width: 2px;
+	border-bottom-width: 2px;
+	border-left-width: 2px;
+	border-top-style: solid;
+	border-right-style: solid;
+	border-bottom-style: solid;
+	border-left-style: solid;
+	border-top-color: #eeeeee;
+	border-right-color: #eeeeee;
+	border-bottom-color: #eeeeee;
+	border-left-color: #eeeeee;
+	border-image-source: initial;
+	border-image-slice: initial;
+	border-image-width: initial;
+	border-image-outset: initial;
+	border-image-repeat: initial;
+	font-size: 16px;
+}
+
+.contact_us_6 .submit-btn {
+	width: 98%;
+	background-color: #03a9f4;
+	height: 60px;
+	font-size: 20px;
+	font-weight: 700;
+	color: white;
+	border-top-width: 0px;
+	border-right-width: 0px;
+	border-bottom-width: 0px;
+	border-left-width: 0px;
+	border-top-style: outset;
+	border-right-style: outset;
+	border-bottom-style: outset;
+	border-left-style: outset;
+	border-top-color: #767676;
+	border-right-color: #767676;
+	border-bottom-color: #767676;
+	border-left-color: #767676;
+	border-image-source: initial;
+	border-image-slice: initial;
+	border-image-width: initial;
+	border-image-outset: initial;
+	border-image-repeat: initial;
+	border-top-left-radius: 40px;
+	border-top-right-radius: 40px;
+	border-bottom-right-radius: 40px;
+	border-bottom-left-radius: 40px;
+}
+
+.contact_us_6 .form-box {
+	z-index: 2;
+	margin-top: 0px;
+	margin-right: 48px;
+	margin-bottom: 0px;
+	margin-left: 0px;
+}
+
+.contact_us_6 .text-blk.input-title {
+	text-align: left;
+	padding-top: 0px;
+	padding-right: 0px;
+	padding-bottom: 0px;
+	padding-left: 10px;
+	font-size: 14px;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 5px;
+	margin-left: 0px;
+	color: #9c9c9c;
+}
+
+.contact_us_6 ::placeholder {
+	color: #dadada;
+}
+
+.contact_us_6 .mob-text {
+	display: block;
+	text-align: left;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 25px;
+	margin-left: 0px;
+}
+
+.contact_us_6 .responsive-cell-block.wk-tab-12.wk-mobile-12.wk-desk-12.wk-ipadp-12 {
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 20px;
+	margin-left: 0px;
+}
+
+.contact_us_6 .text-blk.contactus-subhead.color {
+	color: white;
+}
+
+.contact_us_6 .map-box {
+	max-width: 800px;
+	max-height: 520px;
+	width: 100%;
+	height: 520px;
+	background-color: #d9d9d9;
+	background-image: url("@/assets/Images/image-contact.png");
+	background-size: cover;
+	background-position-x: 50%;
+	background-position-y: 50%;
+}
+
+.contact_us_6 .map-part {
+	width: 100%;
+	height: 100%;
+}
+
+.contact_us_6 .text-blk.map-contactus-head {
+	font-weight: 900;
+	font-size: 22px;
+	line-height: 32px;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 10px;
+	margin-left: 0px;
+	color: #03a9f4;
+}
+
+.contact_us_6 .text-blk.map-contactus-subhead {
+	max-width: 300px;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 20px;
+	margin-left: 0px;
+}
+
+.contact_us_6 .social-media-links.mob {
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 30px;
+	margin-left: 0px;
+	width: 230px;
+	display: flex;
+	justify-content: flex-start;
+}
+
+.contact_us_6 .link-img {
+	width: 30px;
+	height: 30px;
+	margin-top: 0px;
+	margin-right: 25px;
+	margin-bottom: 0px;
+	margin-left: 0px;
+}
+
+.contact_us_6 .link-img.image-block {
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+	margin-left: 0px;
+}
+
+.contact_us_6 .social-icon-link {
+	margin: 0 25px 0 0;
+	padding: 0 0 0 0;
+}
+
+@media (max-width: 1024px) {
+	.contact_us_6 .responsive-container-block.container {
+		justify-content: center;
+	}
+
+	.contact_us_6 .map-box {
+		position: absolute;
+		top: 0px;
+		max-height: 320px;
+	}
+
+	.contact_us_6 .map-box {
+		max-width: 100%;
+		width: 100%;
+	}
+
+	.contact_us_6 .responsive-container-block.container {
+		padding-top: 0px;
+		padding-right: 0px;
+		padding-bottom: 0px;
+		padding-left: 0px;
+	}
+
+	.contact_us_6 .map-part {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	.contact_us_6 .container-block.form-wrapper {
+		margin-top: 0px;
+		margin-right: 0px;
+		margin-bottom: 0px;
+		margin-left: 0px;
+	}
+
+	.contact_us_6 .mob-text {
+		display: block;
+	}
+
+	.contact_us_6 .form-box {
+		margin-top: 200px;
+		margin-right: 60px;
+		margin-bottom: 40px;
+		margin-left: 0px;
+	}
+
+	.contact_us_6 .link-img {
+		margin-top: 0px;
+		margin-right: 0px;
+		margin-bottom: 0px;
+		margin-left: 0px;
+		display: flex;
+		justify-content: space-evenly;
+	}
+
+	.contact_us_6 .social-media-links.mob {
+		justify-content: space-evenly;
+	}
+
+	.contact_us_6 .responsive-cell-block.wk-desk-7.wk-ipadp-12.wk-tab-12.wk-mobile-12 {
+		text-align: center;
+		display: flex;
+		justify-content: flex-end;
+		align-items: center;
+		flex-direction: row;
+	}
+
+	.contact_us_6 .text-blk.contactus-subhead {
+		display: block;
+	}
+
+	.contact_us_6 .mob-text {
+		text-align: center;
+		margin-top: 0px;
+		margin-right: 0px;
+		margin-bottom: 0px;
+		margin-left: 0px;
+	}
+
+	.contact_us_6 .responsive-container-block.container {
+		flex-wrap: wrap;
+	}
+
+	.contact_us_6 .form-box {
+		margin-top: 200px;
+		margin-right: 0px;
+		margin-bottom: 40px;
+		margin-left: 0px;
+	}
+}
+
+@media (max-width: 768px) {
+	.contact_us_6 .submit-btn {
+		width: 100%;
+	}
+
+	.contact_us_6 .input {
+		width: 100%;
+	}
+
+	.contact_us_6 .textinput {
+		width: 100%;
+	}
+
+	.contact_us_6 .container-block.form-wrapper {
+		margin-top: 80px;
+		margin-right: 0px;
+		margin-bottom: 0px;
+		margin-left: 0px;
+	}
+
+	.contact_us_6 .text-blk.input-title {
+		padding-top: 0px;
+		padding-right: 0px;
+		padding-bottom: 0px;
+		padding-left: 0px;
+	}
+
+	.contact_us_6 .form-box {
+		padding-top: 0px;
+		padding-right: 20px;
+		padding-bottom: 0px;
+		padding-left: 20px;
+	}
+
+	.contact_us_6 .container-block.form-wrapper {
+		padding-top: 50px;
+		padding-right: 15px;
+		padding-bottom: 50px;
+		padding-left: 15px;
+	}
+
+	.contact_us_6 .mob-text {
+		display: block;
+	}
+
+	.contact_us_6 .responsive-container-block.container {
+		padding-top: 0px;
+		padding-right: 0px;
+		padding-bottom: 0px;
+		padding-left: 0px;
+	}
+
+	.contact_us_6 .form-box {
+		margin-top: 200px;
+		margin-right: 0px;
+		margin-bottom: 0px;
+		margin-left: 0px;
+	}
+
+	.contact_us_6 .container-block.form-wrapper {
+		margin-top: 0px;
+		margin-right: 0px;
+		margin-bottom: 0px;
+		margin-left: 0px;
+	}
+
+	.contact_us_6 .form-box {
+		margin-top: 220px;
+		margin-right: 0px;
+		margin-bottom: 0px;
+		margin-left: 0px;
+	}
+
+	.contact_us_6 .form-box {
+		margin-top: 220px;
+		margin-right: 0px;
+		margin-bottom: 50px;
+		margin-left: 0px;
+	}
+
+	.contact_us_6 .text-blk.contactus-head {
+		font-size: 32px;
+		line-height: 40px;
+	}
+}
+
+@media (max-width: 500px) {
+	.contact_us_6 .container-block.form-wrapper {
+		padding-top: 50px;
+		padding-right: 15px;
+		padding-bottom: 50px;
+		padding-left: 15px;
+	}
+
+	.contact_us_6 .container-block.form-wrapper {
+		margin-top: 60px;
+		margin-right: 0px;
+		margin-bottom: 0px;
+		margin-left: 0px;
+	}
+
+	.contact_us_6 .responsive-cell-block.wk-ipadp-6.wk-tab-12.wk-mobile-12.wk-desk-6 {
+		margin-top: 0px;
+		margin-right: 0px;
+		margin-bottom: 15px;
+		margin-left: 0px;
+	}
+
+	.contact_us_6 .responsive-container-block {
+		margin-top: 0px;
+		margin-right: 0px;
+		margin-bottom: 35px;
+		margin-left: 0px;
+	}
+
+	.contact_us_6 .text-blk.input-title {
+		font-size: 12px;
+	}
+
+	.contact_us_6 .text-blk.contactus-head {
+		font-size: 26px;
+		line-height: 35px;
+	}
+
+	.contact_us_6 .input {
+		height: 45px;
+	}
+}
+
+
+.wk-desk-1 {
+	width: 8.333333%;
+}
+
+.wk-desk-2 {
+	width: 16.666667%;
+}
+
+.wk-desk-3 {
+	width: 25%;
+}
+
+.wk-desk-4 {
+	width: 33.333333%;
+}
+
+.wk-desk-5 {
+	width: 41.666667%;
+}
+
+.wk-desk-6 {
+	width: 50%;
+}
+
+.wk-desk-7 {
+	width: 58.333333%;
+}
+
+.wk-desk-8 {
+	width: 66.666667%;
+}
+
+.wk-desk-9 {
+	width: 75%;
+}
+
+.wk-desk-10 {
+	width: 83.333333%;
+}
+
+.wk-desk-11 {
+	width: 91.666667%;
+}
+
+.wk-desk-12 {
+	width: 100%;
+}
+
+@media (max-width: 1024px) {
+	.wk-ipadp-1 {
+		width: 8.333333%;
+	}
+
+	.wk-ipadp-2 {
+		width: 16.666667%;
+	}
+
+	.wk-ipadp-3 {
+		width: 25%;
+	}
+
+	.wk-ipadp-4 {
+		width: 33.333333%;
+	}
+
+	.wk-ipadp-5 {
+		width: 41.666667%;
+	}
+
+	.wk-ipadp-6 {
+		width: 50%;
+	}
+
+	.wk-ipadp-7 {
+		width: 58.333333%;
+	}
+
+	.wk-ipadp-8 {
+		width: 66.666667%;
+	}
+
+	.wk-ipadp-9 {
+		width: 75%;
+	}
+
+	.wk-ipadp-10 {
+		width: 83.333333%;
+	}
+
+	.wk-ipadp-11 {
+		width: 91.666667%;
+	}
+
+	.wk-ipadp-12 {
+		width: 100%;
+	}
+}
+
+@media (max-width: 768px) {
+	.wk-tab-1 {
+		width: 8.333333%;
+	}
+
+	.wk-tab-2 {
+		width: 16.666667%;
+	}
+
+	.wk-tab-3 {
+		width: 25%;
+	}
+
+	.wk-tab-4 {
+		width: 33.333333%;
+	}
+
+	.wk-tab-5 {
+		width: 41.666667%;
+	}
+
+	.wk-tab-6 {
+		width: 50%;
+	}
+
+	.wk-tab-7 {
+		width: 58.333333%;
+	}
+
+	.wk-tab-8 {
+		width: 66.666667%;
+	}
+
+	.wk-tab-9 {
+		width: 75%;
+	}
+
+	.wk-tab-10 {
+		width: 83.333333%;
+	}
+
+	.wk-tab-11 {
+		width: 91.666667%;
+	}
+
+	.wk-tab-12 {
+		width: 100%;
+	}
+}
+
+@media (max-width: 500px) {
+	.wk-mobile-1 {
+		width: 8.333333%;
+	}
+
+	.wk-mobile-2 {
+		width: 16.666667%;
+	}
+
+	.wk-mobile-3 {
+		width: 25%;
+	}
+
+	.wk-mobile-4 {
+		width: 33.333333%;
+	}
+
+	.wk-mobile-5 {
+		width: 41.666667%;
+	}
+
+	.wk-mobile-6 {
+		width: 50%;
+	}
+
+	.wk-mobile-7 {
+		width: 58.333333%;
+	}
+
+	.wk-mobile-8 {
+		width: 66.666667%;
+	}
+
+	.wk-mobile-9 {
+		width: 75%;
+	}
+
+	.wk-mobile-10 {
+		width: 83.333333%;
+	}
+
+	.wk-mobile-11 {
+		width: 91.666667%;
+	}
+
+	.wk-mobile-12 {
+		width: 100%;
+	}
+}
+</style>
