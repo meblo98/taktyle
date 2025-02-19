1d7bf9d6d4060986173f27cd3657293b6b756092
meblo_barham <lomouhamedelbachir@gmail.com>
2025-02-19 12:43:46 +0000

    new footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,144 @@
 <template>
-    <footer class="relativemt-5 pt-8 pb-6">
+
+  <footer
+          class="text-center text-lg-start text-dark"
+          style="background-color: #ECEFF1"
+          >
+    <!-- Section: Social media -->
+    <section
+             class="d-flex justify-content-between p-4 text-white"
+             style="background-color: #0a195c"
+             >
+      <!-- Left -->
+      <div class="me-5">
+        <span>Connectez-vous à nous sur les réseaux sociaux:</span>
+      </div>
+      <!-- Left -->
+
+      <!-- Right -->
+      <div>
+        <a href="" class="text-white me-4">
+          <i class="fab fa-facebook-f"></i>
+        </a>
+        <a href="" class="text-white me-4">
+          <i class="fab fa-twitter"></i>
+        </a>
+        <a href="" class="text-white me-4">
+          <i class="fab fa-google"></i>
+        </a>
+        <a href="" class="text-white me-4">
+          <i class="fab fa-instagram"></i>
+        </a>
+        <a href="" class="text-white me-4">
+          <i class="fab fa-linkedin"></i>
+        </a>
+        <a href="" class="text-white me-4">
+          <i class="fab fa-github"></i>
+        </a>
+      </div>
+      <!-- Right -->
+    </section>
+    <!-- Section: Social media -->
+
+    <!-- Section: Links  -->
+    <section class="">
+      <div class="container text-center text-md-start mt-5">
+        <!-- Grid row -->
+        <div class="row mt-3">
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+            <!-- Content -->
+            <h6 class="text-uppercase fw-bold">Taktyl Labs</h6>
+            <hr
+                class="mb-4 mt-0 d-inline-block mx-auto"
+                style="width: 60px; background-color: #7c4dff; height: 2px"
+                />
+            <p>
+             Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi exercitationem amet animi ea ipsam adipisci dicta laborum dignissimos beatae corporis!
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold">Nos Solutions</h6>
+            <hr
+                class="mb-4 mt-0 d-inline-block mx-auto"
+                style="width: 60px; background-color: #7c4dff; height: 2px"
+                />
+            <p>
+              <a href="#!" class="text-dark">Yooko</a>
+            </p>
+            <p>
+              <a href="#!" class="text-dark">Eduxel</a>
+            </p>
+            <p>
+              <a href="#!" class="text-dark">Nafabi</a>
+            </p>
+            <p>
+              <a href="#!" class="text-dark">Leadiz</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold">Liens utiles</h6>
+            <hr
+                class="mb-4 mt-0 d-inline-block mx-auto"
+                style="width: 60px; background-color: #7c4dff; height: 2px"
+                />
+            <p>
+              <a href="#!" class="text-dark">Blog</a>
+            </p>
+            <p>
+              <a href="#!" class="text-dark">Candidature</a>
+            </p>
+            <p>
+              <a href="#!" class="text-dark">À propos de nous</a>
+            </p>
+            <p>
+              <a href="#!" class="text-dark">Help</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold">Contact</h6>
+            <hr
+                class="mb-4 mt-0 d-inline-block mx-auto"
+                style="width: 60px; background-color: #7c4dff; height: 2px"
+                />
+            <p><i class="fas fa-home mr-3"></i> 4 cité Cpi, VDN Dakar</p>
+            <p><i class="fas fa-envelope mr-3"></i> contact@taktylgroup.com</p>
+            <p><i class="fas fa-phone mr-3"></i> +221 77 123 45 67</p>
+          </div>
+          <!-- Grid column -->
+        </div>
+        <!-- Grid row -->
+      </div>
+    </section>
+    <!-- Section: Links  -->
+
+    <!-- Copyright -->
+    <div
+         class="text-center p-3"
+         style="background-color: rgba(0, 0, 0, 0.2)"
+         >
+      © 2020 Copyright:
+      <a class="text-dark" href="#"
+         >TAKTYL LABS</a
+        >
+    </div>
+    <!-- Copyright -->
+  </footer>
+
+
+    <!-- <footer class="relativemt-5 pt-8 pb-6">
       <div class="container mx-auto px-4">
         <div class="flex flex-wrap text-left lg:text-left">
           <div class="w-full lg:w-6/12 px-4">
@@ -98,7 +237,7 @@
           </div>
         </div>
       </div>
-    </footer>
+    </footer> -->
   </template>
   
   <script>
