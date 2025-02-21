0592d8c232afa24520cdc74f480fd9e938a450da
meblo_barham <lomouhamedelbachir@gmail.com>
2025-02-21 11:34:33 +0000

    review de la page login

--- src/views/Admin/LoginView.vue
+++ src/views/Admin/LoginView.vue
@@ -1,17 +1,35 @@
 <template>
-    <div class="container h-screen flex items-center justify-center bg-cover bg-center">
+  <!-- component -->
+  <div class="flex h-screen w-full items-center justify-center bg-gray-900 bg-cover bg-no-repeat"
+    style="background-image:url('https://images.unsplash.com/photo-1499123785106-343e69e68db1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1748&q=80')">
+    <div class="rounded-xl bg-gray-800 bg-opacity-50 px-16 py-10 shadow-lg backdrop-blur-md max-sm:px-8">
+      <div class="text-white">
+        <div class="mb-8 flex flex-col items-center">
+          <img src="@/assets/Images/logo.svg" width="150" alt="" srcset="" />
+          <h1 class="mb-2 text-2xl"></h1>
+          <span class="text-gray-300 mt-5">Saisir les données de connexion</span>
+        </div>
         <form class="form" @submit.prevent="submitForm">
-            <span class="input-span">
-                <label for="email" class="label">Email</label>
-                <input type="email" name="email" id="email" v-model="form.email" required/>
-            </span>
-            <span class="input-span">
-                <label for="password" class="label">Mot de passe</label>
-                <input type="password" name="password" id="password" v-model="form.password" required/>
-            </span>
-            <input class="submit" type="submit" value="Se connecter" />
+          <div class="mb-4 text-lg">
+            <input
+              class="rounded-3xl border-none bg-yellow-400 bg-opacity-50 text-warning px-6 py-2 text-center text-inherit placeholder-slate-200 shadow-lg outline-none backdrop-blur-md"
+              type="text" name="email" id="email" v-model="form.email" placeholder="id@email.com" />
+          </div>
+
+          <div class="mb-4 text-lg">
+            <input
+              class="rounded-3xl border-none bg-yellow-400 bg-opacity-50 text-warning px-6 py-2 text-center text-inherit placeholder-slate-200 shadow-lg outline-none backdrop-blur-md"
+              type="Password" name="password" id="password" v-model="form.password" placeholder="*********" />
+          </div>
+          <div class="mt-8 flex justify-center text-lg text-black">
+            <button type="submit"
+              class="rounded-3xl bg-warning bg-opacity-50 px-5 rounded-full py-2 text-white shadow-xl backdrop-blur-md transition-colors duration-300 hover:bg-yellow-600">Login</button>
+          </div>
         </form>
+      </div>
     </div>
+  </div>
+
 </template>
 
 <script>
@@ -19,119 +37,43 @@ import axios from 'axios';
 import Swal from 'sweetalert2';
 
 export default {
-    data() {
-        return {
-            form: {
-                email: '',
-                password: ''
-            }
-        };
-    },
-    methods: {
-        async submitForm() {
-            console.log(this.form);
-            
-            try {
-                const response = await axios.post('http://127.0.0.1:8000/api/login', this.form);
-                
-                // Vérifier si l'authentification est réussie
-                if (response.data.token) {
-                    localStorage.setItem('token', response.data.token); // Stocker le token
-                    Swal.fire({
-                        icon: 'success',
-                        title: 'Connexion réussie',
-                        text: 'Bienvenue !',
-                        timer: 1500,
-                        showConfirmButton: false
-                    });
-                    // Rediriger vers une autre page (ex: tableau de bord)
-                    this.$router.push('/admin'); 
-                }
-            } catch (error) {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Erreur de connexion',
-                    text: 'Email ou mot de passe incorrect',
-                });
-            }
+  data() {
+    return {
+      form: {
+        email: '',
+        password: ''
+      }
+    };
+  },
+  methods: {
+    async submitForm() {
+
+      try {
+        const response = await axios.post('http://127.0.0.1:8000/api/login', this.form);
+
+        // Vérifier si l'authentification est réussie
+        if (response.data.token) {
+          localStorage.setItem('token', response.data.token); // Stocker le token
+          Swal.fire({
+            icon: 'success',
+            title: 'Connexion réussie',
+            text: 'Bienvenue !',
+            timer: 1500,
+            showConfirmButton: false
+          });
+          // Rediriger vers une autre page (ex: tableau de bord)
+          this.$router.push('/admin');
         }
+      } catch (error) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Erreur de connexion',
+          text: 'Email ou mot de passe incorrect',
+        });
+      }
     }
+  }
 };
 </script>
 
-<style lang="css">
-.form {
-  --bg-light:rgb(255, 188, 2);
-  --bg-dark:rgb(0, 68, 255);
-  --clr:rgb(255, 145, 0);
-  --clr-alpha:rgba(255, 255, 255, 0.38);
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 1rem;
-  width: 100%;
-  max-width: 300px;
-}
-
-.form .input-span {
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-}
-
-.form input[type="email"],
-.form input[type="password"] {
-  border-radius: 0.5rem;
-  padding: 1rem 0.75rem;
-  width: 100%;
-  border: none;
-  display: flex;
-  align-items: center;
-  gap: 0.5rem;
-  background-color: var(--clr-alpha);
-  outline: 2px solid var(--bg-dark);
-}
-
-.form input[type="email"]:focus,
-.form input[type="password"]:focus {
-  outline: 2px solid var(--clr);
-}
-
-.label {
-  align-self: flex-start;
-  color: var(--clr);
-  font-weight: 600;
-}
-
-.form .submit {
-  padding: 1rem 0.75rem;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  gap: 0.5rem;
-  border-radius: 3rem;
-  background-color: var(--bg-dark);
-  color: var(--bg-light);
-  border: none;
-  cursor: pointer;
-  transition: all 300ms;
-  font-weight: 600;
-  font-size: 0.9rem;
-}
-
-.form .submit:hover {
-  background-color: var(--clr);
-  color: var(--bg-dark);
-}
-
-.span {
-  text-decoration: none;
-  color: var(--bg-dark);
-}
-
-.span a {
-  color: var(--clr);
-}
-
-</style>
+<style lang="css" scoped></style>
